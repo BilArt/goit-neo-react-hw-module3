6957e806a8fd4fe1e922ad79062b32643de9186c
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-09-26 16:35:14 +0200

    done ContactForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import ContactList from './components/ContactList/ContactList';
-import SearchBox from './components/SearchBox/SearchBox';
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
 import "./App.css";
 
 function App() {
@@ -10,23 +11,32 @@ function App() {
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ]);
-  
-  const [searchTerm, setSearchTerm] = useState('');
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().split(" ").some(word => 
-      word.startsWith(searchTerm.toLowerCase())
-    )
-  );
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const addContact = (newContact) => {
+    setContacts([...contacts, newContact]); // Добавляем новый контакт в список
+  };
 
   const deleteContact = (id) => {
     setContacts(contacts.filter((contact) => contact.id !== id));
   };
 
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name
+      .toLowerCase()
+      .split(" ")
+      .some((word) => word.startsWith(searchTerm.toLowerCase()))
+  );
+
   return (
     <div>
       <h1>Phonebook</h1>
-      <SearchBox searchTerm={searchTerm} onSearchChange={(e) => setSearchTerm(e.target.value)} />
+      <SearchBox
+        searchTerm={searchTerm}
+        onSearchChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <ContactForm onSubmit={addContact} /> {/* Добавление новой формы */}
       <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </div>
   );
